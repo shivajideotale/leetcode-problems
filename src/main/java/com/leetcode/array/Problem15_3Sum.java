9e2b1ce469422ebe203ba21183cc5f3a8f4a2bb2
SHIVAJI-PC\shiva <shivadeotale@gmail.com>
2025-05-18 10:26:57 +0530

    first commit

---
+++ src/main/java/com/leetcode/array/Problem15_3Sum.java
@@ -0,0 +1,16 @@
+package com.leetcode.array;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/3sum/description/
+ */
+public class Problem15_3Sum {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+
+
+
+}
